7f92587ed3c19e27049c4bde34e4b4515afa65e4
Gergely Szell <sethyx@gmail.com>
2021-01-28 11:21:42 +0100

    Adding readme, some cosmetics and error checks

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # transmission-cleanup
+
+A simple script to remove torrents from Transmission that have been seeding for a long time.
+
+Please respect the torrent etiquette and don't set the seeding threshold to a low value. You can setup your Transmission to mark torrents as finished once they reached the preferred ratio (1+:1). This script can help removing torrents which are not being leeched anymore and therefore probably won't ever get marked as finished.
+
+## Supported tags and Dockerfile links
+
+-	[`latest` (*Dockerfile*)](https://github.com/sethyx/transmission-cleanup/blob/main/Dockerfile)
+
+![](https://img.shields.io/docker/pulls/sethyx/transmission-cleanup "Total docker pulls")
+
+## How it works
+
+- Connects to Transmission RPC to list torrents and get torrent details (supports user+password auth).
+- If a torrent finished downloading and has been seeding for more than the defined threshold, marks it as completed.
+- If a torrent is marked as completed, removes the torrent.
+- Runs periodically.
+
+## How to use
+
+### docker-compose (recommended)
+
+```
+version: '3.4'
+services:
+  transmission-cleanup:
+    image: sethyx/transmission-cleanup:latest
+    container_name: transmission-cleanup
+    environment:
+      - "TRANSMISSION_RPC=host:9091"
+      - "TRANSMISSION_RPC_AUTH=true" # optional, set to true if using auth
+      - "TRANSMISSION_RPC_USER=youruser" # optional
+      - "TRANSMISSION_RPC_PASSWORD=yourpassword" # optional
+      - "RUN_INTERVAL=12" # hours
+      - "SEEDING_THRESHOLD=7" # days
+    restart: unless-stopped
+```
+
+### docker cli
+
+```bash
+docker run -d \
+    -e "TRANSMISSION_RPC=host:9091" \
+    -e "TRANSMISSION_RPC_AUTH=true" \
+    -e "TRANSMISSION_RPC_USER=youruser" \
+    -e "TRANSMISSION_RPC_PASSWORD=yourpassword" \
+    -e "RUN_INTERVAL=12" \
+    -e "SEEDING_THRESHOLD=7" \
+    transmission-cleanup
+```
+
+## Parameters
+
+Container images are configured using parameters passed at runtime (such as those above).
+
+| Parameter | Function |
+| :----: | --- |
+| `-e "TRANSMISSION_RPC=host:9091"` | host and port of Transmission RPC, the default port is 9091 |
+| `-e "TRANSMISSION_RPC_AUTH=true"` | optional, set this to true if your Transmission RPC expects a user and password for authentication |
+| `-e "TRANSMISSION_RPC_USER=youruser"` | optional, username, if you're using auth |
+| `-e "TRANSMISSION_RPC_PASSWORD=youruser"` | optional, password, if you're using auth |
+| `-e "RUN_INTERVAL=12"` | interval to check torrents (hours) |
+| `-e "SEEDING_THRESHOLD=7"` | seeding time (days) that should be considered finished - once reached, the torrent will be marked as finished and then removed in the next run |
+
+
+## Logging
+
+Everything goes to `stdout`.
--- cleanup.sh
+++ cleanup.sh
@@ -1,42 +1,58 @@
 #!/bin/sh
 
-RUN_INT_SECS=$(($RUN_INTERVAL_MINS * 60))
-SEEDING_THRESHOLD_SECONDS=$(($SEEDING_THRESHOLD_HOURS * 3600))
-
-echo "Starting transmission cleanup, RPC: $TRANSMISSION_RPC, running every $RUN_INTERVAL_MINS minutes, with seeding threshold (hours): $SEEDING_THRESHOLD_HOURS"
-
-if [ "$TRANSMISSION_RPC_AUTH" = true ]; then
-    TRANSMISSION_RPC="$TRANSMISSION_RPC -n $TRANSMISSION_RPC_USER:$TRANSMISSION_RPC_PASSWORD"
+# check for mandatory values
+if [[ $(expr "$RUN_INTERVAL" : '[0-9]\+$') -eq 0 ]]; then
+   echo "error: RUN_INTERVAL is not a number, exiting"
+   exit 1
+elif [[ $(expr "$SEEDING_THRESHOLD" : '[0-9]\+$') -eq 0 ]]; then
+   echo "error: SEEDING_THRESHOLD is not a number, exiting"
+   exit 1
 fi
 
+RUN_INT_SECS=$(($RUN_INTERVAL * 3600))
+SEED_THRS_SECS=$(($SEEDING_THRESHOLD * 3600 * 24))
+
+# use auth only if defined
+if [ "$TRANSMISSION_RPC_AUTH" = true ]; then
+    if [ -z "$TRANSMISSION_RPC_USER" ] || [ -z "$TRANSMISSION_RPC_PASSWORD" ]; then
+        echo "error: AUTH is enabled, but user or password is not set, exiting"
+        exit 1
+    fi
+    TRANSMISSION_RPC_FULL="$TRANSMISSION_RPC -n $TRANSMISSION_RPC_USER:$TRANSMISSION_RPC_PASSWORD"
+else
+    TRANSMISSION_RPC_FULL="$TRANSMISSION_RPC"
+fi
+
+echo "Starting transmission cleanup, RPC: $TRANSMISSION_RPC, running every $RUN_INTERVAL hours, with seeding threshold (days): $SEEDING_THRESHOLD"
+
 while true; do
-    # use transmission-remote to get torrent list
-    # use sed to delete first / last line of output and remove leading spaces
-    # use cut to get first field from each line
-    TORRENTLIST=$(transmission-remote $TRANSMISSION_RPC --list | sed -e '1d;$d;s/^ *//' | cut -s -d' ' -f1)
+    # get torrent list
+    TORRENTLIST=$(transmission-remote $TRANSMISSION_RPC_FULL --list | sed -e '1d;$d;s/^ *//' | cut -s -d' ' -f1)
 
     # for each torrent in the list
     for TORRENTID in $TORRENTLIST; do
-        TORRENT_DETAILS=$(transmission-remote $TRANSMISSION_RPC --torrent $TORRENTID --info)
+        TORRENT_DETAILS=$(transmission-remote $TRANSMISSION_RPC_FULL --torrent $TORRENTID --info)
         TORRENT_NAME=$(echo "$TORRENT_DETAILS" | grep -oE 'Name:.+' | cut -d' ' -f2-)
 
         # check seeding time
-        SEEDING_SECONDS=$(echo "$TORRENT_DETAILS" | grep "Seeding Time:" | grep -oE '[0-9]+ seconds' | cut -d' ' -f1 | tail -1)
+        SEEDING_SECONDS=$(echo "$TORRENT_DETAILS" | grep 'Seeding Time:' | grep -oE '[0-9]+ seconds' | cut -d' ' -f1 | tail -1)
 
-        # check if torrent download is completed
-        DL_COMPLETED=$(echo "$TORRENT_DETAILS" | grep "Percent Done: 100%")
+        # check if torrent download completed
+        DL_COMPLETED=$(echo "$TORRENT_DETAILS" | grep 'Percent Done: 100%')
 
-        # check torrents current state is
-        STATE_FINISHED=$(echo "$TORRENT_DETAILS" | grep "State: Finished")
+        # check if torrent state is finished
+        STATE_FINISHED=$(echo "$TORRENT_DETAILS" | grep 'State: Finished')
 
-        # if the torrent finished downloading AND state is "Stopped", "Finished" OR seeding for threshold+
+        # if torrent download completed
         if [ "$DL_COMPLETED" ]; then
+            # if the torrent is marked as finished, remove it
             if [ "$STATE_FINISHED" ]; then
                 echo "Torrent #$TORRENTID ($TORRENT_NAME) is completed, removing"
-                transmission-remote $TRANSMISSION_RPC --torrent $TORRENTID --remove-and-delete
-            elif [ "$SEEDING_SECONDS" -gt "$SEEDING_THRESHOLD_SECONDS" ]; then
+                transmission-remote $TRANSMISSION_RPC_FULL --torrent $TORRENTID --remove-and-delete
+            # if the torrent has been seeding for more than the threshold, mark is as finished
+            elif [ "$SEEDING_SECONDS" -gt "$SEED_THRS_SECS" ]; then
                 echo "Torrent #$TORRENTID ($TORRENT_NAME) has been seeding for more than the defined threshold, marking as finished"
-                transmission-remote $TRANSMISSION_RPC --torrent $TORRENTID -sr 0
+                transmission-remote $TRANSMISSION_RPC_FULL --torrent $TORRENTID -sr 0
             else
                 echo "Torrent #$TORRENTID ($TORRENT_NAME) still in progress, ignoring"
             fi
